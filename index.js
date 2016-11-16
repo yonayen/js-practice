f0f1e4aa3c4a73636f4300b61d80bd93a77c4924
Yonathan Ayenew <yonayen@gmail.com>
2016-11-16 15:20:01 -0800

    add first/last name and age questions with alert of user response

---
+++ index.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?")
+var lastName = prompt("What is your last name?")
+var whatIsAge = prompt("How old are you?")
+
+alert("Your full name is: " + first name + " " + last name)
+alert("You're " + whatIsAge + " years old")
