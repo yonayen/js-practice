f74acbe547bc5685f0dad02dbf006b5adb32f3fe
Yonathan Ayenew <yonayen@gmail.com>
2016-11-16 15:31:04 -0800

    final changes to js and html files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var firstName = prompt("What is your first name?")
-var lastName = prompt("What is your last name?")
-var whatIsAge = prompt("How old are you?")
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var whatIsAge = prompt("How old are you?");
 
-alert("Your full name is: " + first name + " " + last name)
-alert("You're " + whatIsAge + " years old")
+alert("Your full name is " + firstName + " " + lastName);
+alert("You're " + whatIsAge + " years old");
