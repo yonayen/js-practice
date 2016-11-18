bc5682f8cba6d90250083f5fcbc44ae4a83fb41a
Yonathan Ayenew <yonayen@gmail.com>
2016-11-18 15:35:36 -0800

    make operater changes and modifications

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ while (count < str.length) {
 
 var num;
 
-while (num > -10 && num < 19) {
+while (num >= -10 && num <= 19) {
   console.log(num);
   num++;
 }
@@ -61,7 +61,7 @@ while (num > -10 && num < 19) {
 
 var num;
 
-while (num > 10 && num < 40) {
+while (num >= 10 && num <= 40) {
   if (num % 2 === 0) {
     console.log(num);
   }
@@ -72,7 +72,7 @@ while (num > 10 && num < 40) {
 
 var num;
 
-while (num > 300 && num < 333) {
+while (num >= 300 && num <= 333) {
   if (num % 2 !==0 ) {
     console.log(num);
   }
@@ -83,7 +83,7 @@ while (num > 300 && num < 333) {
 
 var num;
 
-while (num > 5 && num < 50) {
+while (num >= 5 && num <= 50) {
   if (num % 5 === 0 && num % 3 === 0) {
     console.log(num);
   }
