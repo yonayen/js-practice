f58493e030b68a2954d88f00f94dca859b42f43c
Yonathan Ayenew <yonayen@gmail.com>
2016-11-18 15:34:20 -0800

    complete while loop problem sets

--- index.js
+++ index.js
@@ -47,3 +47,45 @@ while (count < str.length) {
   console.log(str[count]);
   count++;
 }
+
+// all numbers b/w -10 and 19
+
+var num;
+
+while (num > -10 && num < 19) {
+  console.log(num);
+  num++;
+}
+
+// all even numbers b/w 10 and 40
+
+var num;
+
+while (num > 10 && num < 40) {
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+  num++;
+}
+
+// all odd numbers between 300 and 333
+
+var num;
+
+while (num > 300 && num < 333) {
+  if (num % 2 !==0 ) {
+    console.log(num);
+  }
+  num++;
+}
+
+// all numbers divisble by 5 and 3 between 5 and 50
+
+var num;
+
+while (num > 5 && num < 50) {
+  if (num % 5 === 0 && num % 3 === 0) {
+    console.log(num);
+  }
+  num++;
+}
