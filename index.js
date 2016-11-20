ba6f42c39ac566990d6145e399d6778855cd280c
Yonathan Ayenew <yonayen@gmail.com>
2016-11-19 20:41:13 -0800

    modify final for loop

--- index.js
+++ index.js
@@ -114,8 +114,8 @@ while (num <= 50) {
 
 // -- USING FOR LOOP --//
 
-for (i = 5; i <= 50; i++) {
-  if (i % 5 === 0 && i % 3 === 0) {
-    console.log(i);
-  }
-}
+// for (i = 5; i <= 50; i++) {
+//   if (i % 5 === 0 && i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
