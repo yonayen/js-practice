468b2dbb0a2ab4881b681463eb01eca1a2db9549
Yonathan Ayenew <yonayen@gmail.com>
2016-11-21 15:59:42 -0800

    add 'return' example

--- index.js
+++ index.js
@@ -103,14 +103,14 @@ for (i = 300; i <= 333; i++) {
 
 // all numbers divisble by 5 and 3 between 5 and 50
 
-// var num = 5;
-//
-// while (num <= 50) {
-//   if (num % 5 === 0 && num % 3 === 0) {
-//     console.log(num);
-//   }
-//   num++;
-// }
+var num = 5;
+
+while (num <= 50) {
+  if (num % 5 === 0 && num % 3 === 0) {
+    console.log(num);
+  }
+  num++;
+}
 
 // -- USING FOR LOOP --//
 
@@ -119,3 +119,15 @@ for (i = 5; i <= 50; i++) {
     console.log(i);
   }
 }
+
+// -- RETURN KEYWORD -- //
+// create function that capitalizes first character in a string //
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice.(1);
+}
+
+var city = "paris"; // paris
+var capital = capitalize(city); // Paris
+
+ // Capture the returned value in a variable 'capital' 
