bc1300337ed65d04b8db3aaad3d0187650ca0928
Yonathan Ayenew <yonayen@gmail.com>
2016-11-21 16:02:43 -0800

    add multiple return statements but only one that runs when condition of 'string' is met

--- index.js
+++ index.js
@@ -130,4 +130,17 @@ function capitalize(str) {
 var city = "paris"; // paris
 var capital = capitalize(city); // Paris
 
- // Capture the returned value in a variable 'capital' 
+ // Capture the returned value in a variable 'capital'
+
+ function capitalize(str) {
+   if(typeof str === "number") {
+     return "that's not a string!"
+   }
+   return str.charAt(0).toUpperCase() + str.slice(1);
+ }
+
+ var city = "paris";    // "paris"
+ var capital = capitalize(city);  // "Paris"
+
+ var num = 37;
+ var capital = capitalize(num);  //"that's not a string"
