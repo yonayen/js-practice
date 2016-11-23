c22bab79b136a8ac614917321e5f983344ea487f
Yonathan Ayenew <yonayen@gmail.com>
2016-11-22 16:52:05 -0800

    add forEach method for arrays

--- index.js
+++ index.js
@@ -180,3 +180,14 @@ function iterativeFactorial(num) {
  function kebabToSnake(string) {
 
  }
+
+ // ARRAY Section
+
+ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ var colors = ["red", "orange", "yellow", "green"];
+
+ numbers.forEach(function(color){
+   if(color%3 === 0) {
+     console.log(color);
+   }
+ });
